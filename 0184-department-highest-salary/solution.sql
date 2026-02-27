8fa14d9e256d41eeb9932cd688cd99b03f511370
SuryaMamidala04 <suryamamidala75@gmail.com>
2026-02-27 06:05:32 +0000

    Sync LeetCode submission Runtime - 593 ms (98.39%), Memory - 0.0B (100.00%)

--- 0184-department-highest-salary/solution.sql
+++ 0184-department-highest-salary/solution.sql
@@ -1,7 +1,17 @@
 /* Write your PL/SQL query statement below */
 
-SELECT d.name as Department,e.name as Employee,e.salary as Salary FROM Employee e 
+-- SELECT d.name as Department,e.name as Employee,e.salary as Salary FROM Employee e 
+-- join 
+-- Department d
+-- on e.departmentid = d.id
+-- where salary = (select max(salary) from employee where departmentid =e.departmentid);
+
+with cte as(
+SELECT d.name as Department, e.name as Employee, e.salary as Salary, RANK() OVER(partition by d.name order by salary desc) as rank FROM
+Employee e 
 join 
 Department d
-on e.departmentid = d.id
-where salary = (select max(salary) from employee where departmentid =e.departmentid);
+on e.departmentid = d.id)
+SELECT department,employee,salary from cte where rank = 1;
+
+
