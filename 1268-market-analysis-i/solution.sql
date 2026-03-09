8d3f048608a58655d1cb8d5cd989fe4b62de9f4c
SuryaMamidala04 <suryamamidala75@gmail.com>
2026-03-09 17:15:41 +0000

    Sync LeetCode submission Runtime - 1070 ms (67.01%), Memory - 0.0B (100.00%)

--- 1268-market-analysis-i/solution.sql
+++ 1268-market-analysis-i/solution.sql
@@ -1,6 +1,7 @@
-# Write your MySQL query statement below
-SELECT u.user_id as buyer_id, u.join_date, count(o.order_id) as 'orders_in_2019'
+/* Write your PL/SQL query statement below */
+SELECT u.user_id as buyer_id, TO_CHAR(u.join_date,'YYYY-MM-DD') AS join_date, count(o.order_id) as orders_in_2019
 FROM users u
 LEFT JOIN Orders o
-ON o.buyer_id=u.user_id AND YEAR(order_date)='2019'
-GROUP BY u.user_id;
+ON o.buyer_id=u.user_id;
+AND extract(year from o.order_date) = 2019
+GROUP BY u.user_id, u.join_date;
