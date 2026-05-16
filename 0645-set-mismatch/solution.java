d35293b99f9930a2a3fb94aabc46670d4c2f2f9b
SuryaMamidala04 <suryamamidala75@gmail.com>
2026-05-16 07:25:01 +0000

    Sync LeetCode submission Runtime - 3 ms (68.73%), Memory - 47.4 MB (76.85%)

---
+++ 0645-set-mismatch/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans = new int[2];
+        int i= 0;
+        while(i<nums.length){
+            int index = nums[i]-1;
+            if(nums[i] != nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] != j+1){
+                ans[0] = nums[j];
+                ans[1] = j+1; 
+            }
+        }
+        return ans;
+    }
+}
