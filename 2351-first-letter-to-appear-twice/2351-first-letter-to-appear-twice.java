d50358331472ec8e60d21a3598cb016172a46904
SuryaMamidala04 <suryamamidala75@gmail.com>
2026-08-30 22:56:32 +0530

    Time: 0 ms (100%), Space: 43.2 MB (16.77%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,13 +1,21 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        List<Character> list = new ArrayList<>();
+        // List<Character> list = new ArrayList<>();
+        // for(char ch : s.toCharArray()){
+        //     if(list.contains(ch)){
+        //         list.add(ch);
+        //         break;
+        //     }
+        //     list.add(ch);
+        // }
+        // return list.get(list.size()-1);
+        boolean[] frq = new boolean[26];
         for(char ch : s.toCharArray()){
-            if(list.contains(ch)){
-                list.add(ch);
-                break;
+            if(frq[ch-'a']){
+                return ch;
             }
-            list.add(ch);
+            frq[ch-'a'] = true;
         }
-        return list.get(list.size()-1);
+        return ' ';
     }
 }
