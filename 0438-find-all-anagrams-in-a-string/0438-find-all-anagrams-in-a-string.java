7043dff3dd93b4e374a9797b3b07c2522f7f08cd
SuryaMamidala04 <suryamamidala75@gmail.com>
2026-09-03 23:31:00 +0530

    Time: 10 ms (80.95%), Space: 45.8 MB (99.86%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
+        if(s.length()<p.length()) return new ArrayList<>();
         return solve(s,p);
     }
     static List<Integer> solve(String s, String p){
@@ -12,16 +12,33 @@ class Solution {
             arr1[ch-'a']++;
         }
         // System.out.println(ac);
-        for(int i=0; i<=s.length()-p.length(); i++){
+        // for(int i=0; i<=s.length()-p.length(); i++){
           
-            for(int j=i; j<i+p.length(); j++){
-                arr2[s.charAt(j)-'a']++;
-            }
-            if(Arrays.equals(arr1,arr2)){
-                ans.add(i);
-            }
-            Arrays.fill(arr2,0);
+        //     for(int j=i; j<i+p.length(); j++){
+        //         arr2[s.charAt(j)-'a']++;
+        //     }
+        //     if(Arrays.equals(arr1,arr2)){
+        //         ans.add(i);
+        //     }
+        //     Arrays.fill(arr2,0);
+        // }
+        for(int i=0; i<p.length(); i++){
+            arr2[s.charAt(i)-'a']++;
         }
+        if(Arrays.equals(arr1,arr2)){
+            ans.add(0);
+        }
+
+        for(int i=p.length(); i<s.length(); i++){
+            arr2[s.charAt(i)-'a']++;
+            arr2[(s.charAt(i-p.length())-'a')]--;
+            if(Arrays.equals(arr1,arr2)){
+                ans.add(i-p.length()+1);
+            }
+        }
+
+
+
         return ans;
     }
 }
